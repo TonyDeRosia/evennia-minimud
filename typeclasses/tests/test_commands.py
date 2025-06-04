6a52df9aad6ca3554a5ca4c263af57f9a6a537c0
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-04 16:55:31 -0400

    Add player info commands

---
+++ typeclasses/tests/test_commands.py
@@ -0,0 +1,48 @@
+"""Tests for custom commands."""
+
+from unittest.mock import MagicMock
+
+from evennia.utils.test_resources import EvenniaTest
+
+
+class TestInfoCommands(EvenniaTest):
+    def setUp(self):
+        super().setUp()
+        self.char1.msg = MagicMock()
+        self.char2.msg = MagicMock()
+        self.obj1.location = self.char1
+        self.char1.db.desc = "A tester."
+        self.char2.db.desc = "Another tester."
+
+    def test_desc_set_and_view(self):
+        self.char1.execute_cmd("desc")
+        self.assertTrue(self.char1.msg.called)
+        self.char1.msg.reset_mock()
+        self.char1.execute_cmd("desc New description")
+        self.assertEqual(self.char1.db.desc, "New description")
+
+    def test_finger(self):
+        self.char1.execute_cmd(f"finger {self.char2.key}")
+        self.assertTrue(self.char1.msg.called)
+
+    def test_inventory(self):
+        self.char1.execute_cmd("inventory")
+        self.assertTrue(self.char1.msg.called)
+        self.char1.msg.reset_mock()
+        self.obj1.location = self.char1
+        self.char1.execute_cmd("inventory")
+        self.assertTrue(self.char1.msg.called)
+
+    def test_equipment(self):
+        self.char1.attributes.add("_wielded", {"left": self.obj1})
+        self.char1.execute_cmd("equipment")
+        self.assertTrue(self.char1.msg.called)
+
+    def test_buffs(self):
+        self.char1.execute_cmd("buffs")
+        self.assertTrue(self.char1.msg.called)
+        self.char1.msg.reset_mock()
+        self.char1.tags.add("speed", category="buff")
+        self.char1.execute_cmd("buffs")
+        self.assertTrue(self.char1.msg.called)
+
