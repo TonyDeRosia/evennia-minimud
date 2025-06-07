1b1a11ecfe4905f4f8dc435281f36569bdaa4d2c
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-07 05:44:22 -0400

    Remove tickable tag from PC and update tests

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -653,7 +653,6 @@ class PlayerCharacter(Character):
         super().at_object_creation()
         # initialize hands
         self.db._wielded = {"left": None, "right": None}
-        self.tags.add("tickable")
 
     def get_display_name(self, looker, **kwargs):
         """
