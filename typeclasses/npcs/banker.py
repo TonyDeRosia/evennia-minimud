8e50f7bf71a83933cb7c09509b8156e5761afa12
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-08 03:56:56 -0400

    Add NPC subclasses and builder support

---
+++ typeclasses/npcs/banker.py
@@ -0,0 +1,7 @@
+from . import BaseNPC
+
+
+class BankerNPC(BaseNPC):
+    """NPC that handles currency transactions or storage."""
+
+    pass
---
+++ typeclasses/npcs/trainer.py
@@ -0,0 +1,7 @@
+from . import BaseNPC
+
+
+class TrainerNPC(BaseNPC):
+    """NPC that teaches skills or abilities."""
+
+    pass
