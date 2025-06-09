38649023423018c81efdb65c13d08061625a8781
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-08 23:46:06 -0400

    Add tests for mob prototype commands

---
+++ typeclasses/tests/test_mcreate_command.py
@@ -0,0 +1,32 @@
+from unittest.mock import MagicMock
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from unittest import mock
+
+from django.test import override_settings
+from evennia.utils.test_resources import EvenniaTest
+
+from commands.admin import BuilderCmdSet
+from world import prototypes
+
+
+@override_settings(DEFAULT_HOME=None)
+class TestMCreateCommand(EvenniaTest):
+    def setUp(self):
+        super().setUp()
+        self.char1.msg = MagicMock()
+        self.char1.cmdset.add_default(BuilderCmdSet)
+        self.tmp = TemporaryDirectory()
+        patcher = mock.patch.object(
+            prototypes,
+            "_NPC_PROTO_FILE",
+            Path(self.tmp.name) / "npcs.json",
+        )
+        self.addCleanup(self.tmp.cleanup)
+        self.addCleanup(patcher.stop)
+        patcher.start()
+
+    def test_mcreate_creates_prototype(self):
+        self.char1.execute_cmd("@mcreate orc")
+        reg = prototypes.get_npc_prototypes()
+        assert "orc" in reg
---
+++ typeclasses/tests/test_shop_repair_commands.py
@@ -0,0 +1,39 @@
+from unittest.mock import MagicMock
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from unittest import mock
+
+from django.test import override_settings
+from evennia.utils.test_resources import EvenniaTest
+
+from commands.admin import BuilderCmdSet
+from world import prototypes
+
+
+@override_settings(DEFAULT_HOME=None)
+class TestShopRepairCommands(EvenniaTest):
+    def setUp(self):
+        super().setUp()
+        self.char1.msg = MagicMock()
+        self.char1.cmdset.add_default(BuilderCmdSet)
+        self.tmp = TemporaryDirectory()
+        patcher = mock.patch.object(
+            prototypes,
+            "_NPC_PROTO_FILE",
+            Path(self.tmp.name) / "npcs.json",
+        )
+        self.addCleanup(self.tmp.cleanup)
+        self.addCleanup(patcher.stop)
+        patcher.start()
+
+    def test_shop_and_repair_setup(self):
+        self.char1.execute_cmd("@mcreate merchant")
+        self.char1.execute_cmd("@makeshop merchant")
+        self.char1.execute_cmd("@shopset merchant buy 120")
+        self.char1.execute_cmd("@makerepair merchant")
+        self.char1.execute_cmd("@repairset merchant cost 200")
+        reg = prototypes.get_npc_prototypes()
+        shop = reg["merchant"]["shop"]
+        repair = reg["merchant"]["repair"]
+        assert shop["buy_percent"] == 120
+        assert repair["cost_percent"] == 200
