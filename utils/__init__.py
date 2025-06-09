2df575a001b9bb71caf361932ecd4338e86edacc
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-09 04:18:46 -0400

    Add menu utils helper and refactor builder

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .roles import has_role, is_guildmaster, is_receptionist
 
 from .slots import VALID_SLOTS, normalize_slot
 from .ansi_utils import format_ansi_title
+from .menu_utils import add_back_skip
