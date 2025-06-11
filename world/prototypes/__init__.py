58c2eb16f4d1c1626e4e184e2fca4ced9bd9d9b0
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-11 09:48:40 -0400

    Export normalize function

--- world/prototypes/__init__.py
+++ world/prototypes/__init__.py
@@ -12,7 +12,12 @@ spec = spec_from_file_location("world._legacy_prototypes", _legacy_path)
 _legacy = module_from_spec(spec)
 spec.loader.exec_module(_legacy)
 
+if hasattr(_legacy, "_normalize_proto"):
+    _normalize_proto = _legacy._normalize_proto
+
 for _name in [n for n in dir(_legacy) if not n.startswith("_")]:
     globals()[_name] = getattr(_legacy, _name)
 
 __all__ = [n for n in globals() if not n.startswith("_")]
+if "_normalize_proto" in globals():
+    __all__.append("_normalize_proto")
