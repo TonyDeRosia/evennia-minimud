4c5dbc68d80c15ce8a70cfed9b4fc7400b01960d
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-13 15:52:24 -0400

    Revert "Fix logger debug calls"

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -1109,9 +1109,7 @@ class NPC(Character):
             if script and script[0].pk:
                 script[0].remove_combatant(self)
 
-        logger.log_info(
-            "NPC %s killed by %s", self.key, getattr(attacker, "key", None)
-        )
+        logger.debug("NPC %s killed by %s", self.key, getattr(attacker, "key", None))
 
         corpse = make_corpse(self)
         if corpse:
