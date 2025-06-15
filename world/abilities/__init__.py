027fb6379ca49526429aada34646410cd9887c3c
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-15 05:18:13 -0400

    Add class ability table

--- world/abilities/__init__.py
+++ world/abilities/__init__.py
@@ -2,9 +2,10 @@
 
 from .base import Ability
 from .bash import Bash
+from .ability_table import CLASS_ABILITY_TABLE
 
 ABILITY_REGISTRY = {
     Bash.name: Bash,
 }
 
-__all__ = ["Ability", "Bash", "ABILITY_REGISTRY"]
+__all__ = ["Ability", "Bash", "ABILITY_REGISTRY", "CLASS_ABILITY_TABLE"]
