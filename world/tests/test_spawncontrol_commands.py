ccca6dc82b64525664d384064898d481b1265767
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-17 01:25:39 -0400

    Use ScriptDB in spawncontrol

---
+++ world/tests/test_spawncontrol_commands.py
@@ -0,0 +1,28 @@
+from unittest import TestCase, mock
+from commands.admin.spawncontrol import CmdSpawnReload, CmdForceRespawn
+
+
+class TestSpawnControlCommands(TestCase):
+    def test_spawnreload_calls_script(self):
+        cmd = CmdSpawnReload()
+        cmd.caller = mock.Mock()
+        cmd.args = ""
+        cmd.msg = mock.Mock()
+        with mock.patch("commands.admin.spawncontrol.ScriptDB") as mock_sdb:
+            script = mock.Mock()
+            mock_sdb.objects.filter.return_value.first.return_value = script
+            cmd.func()
+            script.reload_spawns.assert_called_once()
+            cmd.msg.assert_called_with("Spawn entries reloaded from prototypes.")
+
+    def test_force_respawn_calls_script(self):
+        cmd = CmdForceRespawn()
+        cmd.caller = mock.Mock()
+        cmd.args = "5"
+        cmd.msg = mock.Mock()
+        with mock.patch("commands.admin.spawncontrol.ScriptDB") as mock_sdb:
+            script = mock.Mock()
+            mock_sdb.objects.filter.return_value.first.return_value = script
+            cmd.func()
+            script.force_respawn.assert_called_with(5)
+            cmd.msg.assert_called_with("Respawn check run for room 5.")
