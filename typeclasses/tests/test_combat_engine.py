304e2f312b07166a8e6c0bfd76c81e5bc0057f67
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-20 12:06:58 -0400

    Add test for killing multiple NPCs

--- typeclasses/tests/test_combat_engine.py
+++ typeclasses/tests/test_combat_engine.py
@@ -570,6 +570,57 @@ class TestCombatDeath(EvenniaTest):
         self.assertNotIn(npc, [p.actor for p in engine.participants])
         self.assertEqual(corpse.db.corpse_of, npc.key)
 
+    def test_player_kills_multiple_npcs_creates_multiple_corpses_and_awards_xp(self):
+        """Killing two NPCs should produce two corpses, loot, and combined XP."""
+        from evennia.utils import create
+        from typeclasses.characters import NPC
+
+        player = self.char1
+        player.db.experience = 0
+
+        npc1 = create.create_object(NPC, key="mob1", location=self.room1)
+        npc2 = create.create_object(NPC, key="mob2", location=self.room1)
+        for npc in (npc1, npc2):
+            npc.db.drops = []
+        npc1.db.exp_reward = 5
+        npc2.db.exp_reward = 7
+
+        loot1 = create.create_object("typeclasses.objects.Object", key="loot1", location=npc1)
+        loot2 = create.create_object("typeclasses.objects.Object", key="loot2", location=npc2)
+
+        engine = CombatEngine([player, npc1, npc2], round_time=0)
+        engine.queue_action(player, KillAction(player, npc1))
+
+        with patch("world.system.state_manager.apply_regen"), patch(
+            "world.system.state_manager.check_level_up"
+        ), patch("random.randint", return_value=0):
+            engine.start_round()
+            engine.process_round()
+
+        engine.queue_action(player, KillAction(player, npc2))
+
+        with patch("world.system.state_manager.apply_regen"), patch(
+            "world.system.state_manager.check_level_up"
+        ), patch("random.randint", return_value=0):
+            engine.process_round()
+
+        corpses = [
+            obj
+            for obj in self.room1.contents
+            if obj.is_typeclass("typeclasses.objects.Corpse", exact=False)
+        ]
+
+        self.assertEqual(len(corpses), 2)
+
+        corpse1 = next(c for c in corpses if c.db.corpse_of == npc1.key)
+        corpse2 = next(c for c in corpses if c.db.corpse_of == npc2.key)
+
+        self.assertIn(loot1, corpse1.contents)
+        self.assertIn(loot2, corpse2.contents)
+
+        total_xp = npc1.db.exp_reward + npc2.db.exp_reward
+        self.assertEqual(player.db.experience, total_xp)
+
 
 class TestCombatNPCTurn(EvenniaTest):
     def test_at_combat_turn_auto_attack(self):
