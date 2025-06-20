e3fca041944232d0ac18ee99d3d09b1493bb353e
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2025-06-20 18:48:23 -0400

    Add Disarm skill and tests

--- world/skills/__init__.py
+++ world/skills/__init__.py
@@ -1,6 +1,7 @@
 from .skill import Skill
 from .kick import Kick
+from .disarm import Disarm
 from .unarmed_passive import Unarmed
 from .hand_to_hand import HandToHand
 
-__all__ = ["Skill", "Kick", "Unarmed", "HandToHand"]
+__all__ = ["Skill", "Kick", "Disarm", "Unarmed", "HandToHand"]
